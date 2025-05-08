cffcf32f2bd055e180716f155d2b84ad75e9bc40
Jonas <jonas.backline@gmail.com>
2025-05-08 21:10:13 +0000

    relationships

--- src/models.py
+++ src/models.py
@@ -1,10 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, Table, relationship
 
 # Creamos una instancia de SQLAlquemy para gestionar la conexión 
 # y los modelos de la base de datos
 db = SQLAlchemy()
+
+# Tabla de asociacion para la relacion "muchos a muchos" entre people y films
+people_films = Table(
+    "people_films",
+    db.model.metadata,
+    db.Column("people_id", db.Integer, db.ForeignKey("people.id"), primary_key=True),
+    db.Column("film_id", db.Integer, db.ForeignKey("film.id"), primary_key=True)
+)
+
+# Tabla de asociacion para la relacion "muchos a muchos" entre planets y films
+planet_films = Table(
+    "planet_films",
+    db.model.metadata,
+    db.Column("planet_id", db.Integer, db.ForeignKey("planet.id"), primary_key=True),
+    db.Column("film_id", db.Integer, db.ForeignKey("film.id"), primary_key=True)
+)
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -31,6 +48,22 @@ class Planet(db.Model):
     terrain: Mapped[str] = mapped_column(String(50), nullable=True)
     population: Mapped[str] = mapped_column(String(50), nullable=True)
     
+    # relación de "uno a muchos": un planeta tiene muchos residentes (personas(people))
+    # People es el nombre de la clase destino
+    # back_populates="homeworld" --> indica que la realción es bidireccional: 
+    # en la clase People debe haber una columna homeworld con su propio relationchip apuntando de regreso a Planet
+    # cascade="all", delete" --> indica que si eliminas un planeta, automáticamente...
+    # ...se eliminarán todos los residentes asociados a ese planeta en la base de datos
+    residents = relationship("People", back_populates="homeworld", cascade="all, delete")
+
+    # relación de "muchos a muchos": un planeta aparece en muchas películas (films)
+    # Film es el nombre de la clase destino
+    # secondary=planet_films -> Aqui le dices a SQLAlquemy que la realción entre Planet y Film es "muchos a muchos"
+    # y usa una tabla intermedia llamada 'planet_films'
+    # back_populates="planets" --> indica que la realción es bidireccional: 
+    # en la clase Film debe haber una columna planet con su propio relationchip apuntando de regreso a Planet
+    films = relationship("Film", secondary=planet_films, back_populates="planets")
+
     def serialize(self):
         # conviete el objeto planeta en un diccionario para poder enviarlo en formato JSON.
         return {
